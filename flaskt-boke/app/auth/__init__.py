74ca749357418fb85be2ef448b9d03bb8e841de1
GZ2007YR <3439014049@qq.com>
2020-06-19 09:37:53 +0800

    修改名称

--- flaskt-boke/app/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-#app/auth/__init__.py
-from flask import Blueprint
-
-auth =Blueprint('auth',__name__)
-from . import forms,views
-
--- flaskt-boke/app/main/__init__.py
+++
@@ -1,6 +0,0 @@
-
-#app/main/__init__.py
-from flask import Blueprint
-
-main =Blueprint('main',__name__)
-from . import views
